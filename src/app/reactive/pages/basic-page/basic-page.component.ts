78615fa365a39cd4d856ee2ac783b1f37cb73b1b
AsusMiguel <migalvez2002@gmail.com>
2024-04-04 19:05:02 -0500

    project to learn reactive forms

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './basic-page.component.html',
+  styles: ``
+})
+export class BasicPageComponent {
+
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // });
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required, Validators.minLength(3) ]],
+    price: [0, [ Validators.required, Validators.min(0) ]],
+    inStorage: [0, [ Validators.required, Validators.min(0) ]],
+  })
+
+  constructor( private fb: FormBuilder ){}
+
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for ( const key of Object.keys(errors) ){
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return 'El campo debe tener minimo 3 letras';
+      }
+    }
+    return null;
+    
+  }
+
+  onSave(){
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched(); 
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset({ price: 0, inStorage: 0 });
+  }
+}
